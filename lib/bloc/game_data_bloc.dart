4b1c0d850b7542ec954b3a1cca275508d8741d30
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-02-18 23:50:44 +0100

    Completed GameDataBloc class.

--- lib/bloc/game_data_bloc.dart
+++ lib/bloc/game_data_bloc.dart
@@ -1,14 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:gamer_power/models/data_model.dart';
+import 'package:gamer_power/repos/games_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'game_data_event.dart';
 part 'game_data_state.dart';
 
 class GameDataBloc extends Bloc<GameDataEvent, GameDataState> {
-  GameDataBloc() : super(GameDataInitialState()) {
-    on<GameDataEvent>((event, emit) {
-      // TODO: implement event handler
+  final GamesRepo gamesRepo;
+  late List<DataModel>? _apiResult;
+
+  GameDataBloc(this.gamesRepo) : super(GameDataInitialState()) {
+    on<GameDataEvent>((event, emit) async {
+      switch (event.runtimeType) {
+        case LoadGameDataEvent:
+          emit(GameDataLoadingState());
+          _apiResult = await gamesRepo.getGames();
+
+          if (_apiResult == null) {
+            emit(GameDataErrorState());
+          } else {
+            emit(GameDataLoadedState(apiResult: _apiResult!));
+          }
+          break;
+        default:
+          emit(GameDataInitialState());
+          break;
+      }
     });
   }
 }
